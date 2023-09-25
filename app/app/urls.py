7f3da2afc8809b73746ac30e052efe883f194bf4
amitkumrsingh <aksingh1109@gmail.com>
2023-09-25 10:30:52 +0530

    fixed indentation

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,4 +1,6 @@
-"""app URL Configuration The `urlpatterns` list routes URLs to views. For more information please see:
+"""app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
@@ -9,12 +11,11 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))"""
-
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
